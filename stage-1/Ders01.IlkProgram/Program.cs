38714dcb1de8c8f9fbbd771be46cdd52662798c0
poyrazavsever <poyrazavsever@hotmail.com>
2026-08-13 12:33:00 +0300

    Add NuGet support files for Ders01.IlkProgram project
    - Created `Ders01.IlkProgram.csproj.nuget.g.props` to define NuGet properties.
    - Added `Ders01.IlkProgram.csproj.nuget.g.targets` for build targets.
    - Generated `project.assets.json` to manage project dependencies and assets.
    - Included `project.nuget.cache` for caching NuGet package information.

---
+++ stage-1/Ders01.IlkProgram/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("What is your name? ");
+string? name = Console.ReadLine();
+
+Console.Write($"Where do you live, {name}? ");
+string? city = Console.ReadLine();
+
+Console.Write($"Why are you learning C#, {name}? ");
+string? reason = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine(
+    $"Hello {name}, you live in {city} and you are learning C# because {reason}."
+);
