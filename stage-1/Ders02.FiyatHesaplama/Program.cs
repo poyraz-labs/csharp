5b507646fcaedd73fda22d1c7e637b74c7b12a05
poyrazavsever <poyrazavsever@hotmail.com>
2026-08-13 12:59:04 +0300

    feat: added ders2 to stage1

---
+++ stage-1/Ders02.FiyatHesaplama/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Kullanıcı adınızı giriniz: ");
+string? kullaniciAdi = Console.ReadLine();
+
+Console.Write("Hangi şehire seyehat etmek istiyorsunuz? ");
+string? sehir = Console.ReadLine();
+
+Console.Write("Kaç gün seyahat edeceksiniz? ");
+int gun = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Günlük konaklama ücreti ne kadar? ");
+decimal konaklamaUcreti = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Günlük yemek ücreti ne kadar? ");
+decimal yemekUcreti = Convert.ToDecimal(Console.ReadLine());
+
+decimal toplamKonaklamaUcreti = konaklamaUcreti * gun;
+decimal toplamYemekUcreti = yemekUcreti * gun;
+decimal toplamUcret = toplamKonaklamaUcreti + toplamYemekUcreti;
+
+Console.WriteLine();
+Console.WriteLine($"========Seyehat Bilgileri========");
+Console.WriteLine($"Kullanıcı Adı: {kullaniciAdi}");
+Console.WriteLine($"Seyehat Edilecek Şehir: {sehir}");
+Console.WriteLine($"Seyehat Süresi: {gun} gün");
+Console.WriteLine($"Toplam konaklama ücreti: {toplamKonaklamaUcreti:F2}");
+Console.WriteLine($"Toplam yemek ücreti: {toplamYemekUcreti:F2}");
+Console.WriteLine($"Toplam ücret: {toplamUcret:F2}");
